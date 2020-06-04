aa16b6b8fe5a67e197a5a2221ffdf9e66bf6469f
dominicrobertsgit <66412943+dominicrobertsgit@users.noreply.github.com>
2020-06-04 09:53:13 +0100

    Added version 1, uses umlet
    Could do with some cleaning up

---
+++ bob.py
@@ -0,0 +1,8 @@
+print("bob.py was just imported")
+
+def ben():
+  print("ben")
+
+class Jim:
+  def jane(self, hey):
+    print("jane")
---
+++ job/jobbo.py
@@ -0,0 +1,15 @@
+class Jobbo:
+  def do(self, hey, there, I, just):
+    print("jerked")
+    self._call_the_bob()
+    self._call_the_bob_differently()
+    
+  def _call_the_bob(self):
+    from bob import Jim
+    j = Jim()
+    j.jane(1)
+    
+  def _call_the_bob_differently(self):
+    import bob
+    j = bob.Jim()
+    j.jane(2)
